11a5e35c493bc01a431ddd4d0abd3a46f267f805
Neil Stephenson <stephenson.n@gmail.com>
2023-08-01 17:48:16 +0100

    Add new to-do items to the list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     return render_template('index.html', items=get_items())
+
+@app.route('/', methods=(['POST']))
+def addItem():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect(url_for('index'))
