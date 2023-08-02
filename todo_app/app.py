f2171f596e9cc40130a833b677f24cd662e00587
Neil Stephenson <stephenson.n@gmail.com>
2023-08-02 15:57:46 +0100

    Marking an item as done

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -12,7 +12,19 @@ def index():
     return render_template('index.html', items=get_items())
 
 @app.route('/', methods=(['POST']))
-def addItem():
-    title = request.form.get('title')
-    add_item(title)
-    return redirect(url_for('index'))
+def update():
+    done_items = request.form.getlist('done')
+    print(done_items)
+    if len(done_items) == 0:
+        title = request.form.get('title')
+        add_item(title)
+        return redirect(url_for('index'))
+    else:
+        print(done_items)
+        for item in done_items[0:-1]:
+            updated_item = get_item(item)
+            updated_item.update({"status": "Done"})
+            save_item(updated_item)
+            print(updated_item)
+        return redirect(url_for('index'))
+    
