647f7ef18d19a0bbfa0809945c8da5b367868238
Neil Stephenson <stephenson.n@gmail.com>
2023-08-04 15:47:59 +0100

    Cosmetic updates and readme instructions

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,8 +1,8 @@
 from flask import session
 
 _DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    { 'id': 1, 'status': 'Not Started', 'title': 'Write a song for a stranger' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Make an exotic sandwich' }
 ]
 
 
