7d0184f35df0d24d405ec2179192faf0d7b7d27d
Neil Stephenson <stephenson.n@gmail.com>
2023-09-29 10:02:39 +0100

    Created first test for done items

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -5,3 +5,11 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def done_items(self):
+        done_item_list = []
+        for item in self._items:
+            if item.status == 'Done':
+                done_item_list.append(item)
+        return done_item_list
