f1efd95b15196120ed96ac8288b97d4ca086e1f4
Neil Stephenson <stephenson.n@gmail.com>
2023-09-29 16:48:56 +0100

    Created integration test

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,31 +4,34 @@ from todo_app.data.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-@app.route('/')
-def index():
-    items = get_items()
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
+    @app.route('/')
+    def index():
+        items = get_items()
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    title = request.form.get('title')
-    add_item(title)
-    return redirect(url_for('index'))
+    @app.route('/add', methods=['GET', 'POST'])
+    def add():
+        title = request.form.get('title')
+        add_item(title)
+        return redirect(url_for('index'))
 
-@app.route('/done', methods=['GET', 'POST'])
-def done():
-    done_items = request.form.getlist('action')
-    for card_id in done_items:
-        update_as_done(card_id)
-    return redirect(url_for('index'))
+    @app.route('/done', methods=['GET', 'POST'])
+    def done():
+        done_items = request.form.getlist('action')
+        for card_id in done_items:
+            update_as_done(card_id)
+        return redirect(url_for('index'))
 
-@app.route('/delete', methods=(['POST']))
-def delete():
-    deleted_items = request.form.getlist('action')
-    for card_id in deleted_items:
-        delete_item(card_id)
-    return redirect(url_for('index'))
+    @app.route('/delete', methods=(['POST']))
+    def delete():
+        deleted_items = request.form.getlist('action')
+        for card_id in deleted_items:
+            delete_item(card_id)
+        return redirect(url_for('index'))
+    
+    return app
---
+++ todo_app/test/test_client.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv, find_dotenv
+from todo_app import app
+import pytest
+import requests
+import os
+
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    # Create the new app.
+    test_app = app.create_app()
+
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
+
+class StubResponse():
+    def __init__(self, fake_response_data):
+        self.fake_response_data = fake_response_data
+
+    def json(self):
+        return self.fake_response_data
+
+def stub(url, params={}):
+    test_board_id = os.environ.get('TRELLO_BOARD_ID')
+
+    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
+        fake_response_data = [{
+            'id': '1a2b3c',
+            'name': 'To Do',
+            'cards': [{'id': '123', 'name': 'Test card 1'},{'id': '456', 'name': 'Test card 2'}]
+        }]
+        return StubResponse(fake_response_data)
+
+    raise Exception(f'Integration test did not expect URL "{url}"')
+
+def test_index_page(monkeypatch, client):
+    # Replace requests.get(url) with our own function
+    monkeypatch.setattr(requests, 'get', stub)
+
+    # Make a request to our app's index page
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert '123' in response.data.decode()
+    assert 'Test card 2' in response.data.decode()
